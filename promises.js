23738807e5f674cf762756a21846f067a69e2b43
ManikanthGaddam <mani95037@gmail.com>
2026-03-24 21:00:59 +0530

    simulating the previous operation using promises

---
+++ promises.js
@@ -0,0 +1,77 @@
+function random(max){
+    return Math.floor(Math.random() * max);
+};
+
+function download(source){
+    return new Promise(function executor(resolve,reject){
+        console.log(`download from ${source} started ... `);
+        setTimeout(function f1 (){
+            const data = "Virat kohli is the GOAT";
+            let num = random(10);
+            if(num % 2 == 0){
+                console.log("download complete");
+                resolve(data);
+            } else {
+                reject("download failed");
+            }
+        },5000);
+    });
+};
+
+function process(data){
+    return new Promise(function executor(resolve,reject){
+        console.log(`processing data...`);
+        setTimeout(function f2(){
+            let num = random(5);
+            if(num % 2 == 0){
+                console.log(`data processed`);
+                resolve(data);
+            } else{
+                reject("data processing stopped due to unexpected error :(");
+            }
+        },3000);
+    });
+};
+
+function writeToFile(data){
+    return new Promise(function executor(resolve,reject) {
+        console.log("writing data...");
+        setTimeout(function f3(){
+            let num = random(7);
+            if(num % 2 == 0){
+                const filename = "cricket.txt";
+                console.log(`${data} is written to ${filename}`);
+                resolve(filename);
+            } else {
+                reject("writing data failed :(");
+            }
+        },5000);
+    });
+};
+
+function upload(filename){
+    return new Promise(function executor(resolve,reject){
+        console.log("uploading file...");
+        setTimeout(function f4(){
+            let num = random(8);
+            if(num % 2 == 0){
+                const newUrl = "www.drive.google.com";
+                console.log(`${filename} has been uploaded on ${newUrl}`);
+                resolve("OPERATION SUCCESSFUL");
+            } else {
+                reject("Uploading the file has failed :(");
+            }
+        },7000);
+    });
+};
+
+download("www.trial.com")
+.then(process)
+.then(writeToFile)
+.then(upload)
+.then(function(status){
+    console.log(status);
+})
+.catch(function (err){
+    console.log("error is : ", err);
+})
